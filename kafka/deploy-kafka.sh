d58e0b3188e27c6ee07252e4f391781d19a312f2
Fabien Denoyelle <denoyelle.fabien@gmail.com>
2026-01-21 12:02:51 +0100

    Add KUBECONFIG export to deploy-kafka.sh
    Set KUBECONFIG environment variable for Kubernetes access.

--- kafka/deploy-kafka.sh
+++ kafka/deploy-kafka.sh
@@ -1,3 +1,5 @@
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
 helm repo add strimzi https://strimzi.io/charts/
 helm repo update
 helm upgrade --install strimzi-kafka strimzi/strimzi-kafka-operator -f strimzi-operator-values.yaml # install kafka operator
