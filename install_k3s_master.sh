8474a0b8cbc008338525be15aa24d2f363913156
Fabien Denoyelle <denoyelle.fabien@gmail.com>
2026-01-22 09:11:08 +0100

    Add uninstall checks for k3s before installation
    Add uninstall commands for k3s agent and server

--- install_k3s_master.sh
+++ install_k3s_master.sh
@@ -1,2 +1,11 @@
+# Désinstallation si présente
+if [ -f /usr/local/bin/k3s-agent-uninstall.sh ]; then
+  sudo /usr/local/bin/k3s-agent-uninstall.sh
+fi
+
+if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
+  sudo /usr/local/bin/k3s-uninstall.sh
+fi
+
 curl -sfL https://get.k3s.io | sh -
 
