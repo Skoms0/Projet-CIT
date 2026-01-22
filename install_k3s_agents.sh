f20e8bfcb941796135765ec5a103ad76d696430e
Fabien Denoyelle <denoyelle.fabien@gmail.com>
2026-01-22 08:48:48 +0100

    Modified install agent script

--- install_k3s_agents.sh
+++ install_k3s_agents.sh
@@ -1,63 +1,50 @@
-#!/bin/bash
-
-# Utilisateur SSH
-USER="root"
-
-# IPs des Raspberry Pi
-HOSTS=(
-  10.0.1.13
-  10.0.1.52
-  10.0.1.53
-  10.0.1.54
-  10.0.1.55
-)
-
-# Lecture du token depuis le fichier
 TOKEN_FILE="k3s_token.txt"
+K3S_URL="https://10.0.1.56:6443"
 
 if [[ ! -f "$TOKEN_FILE" ]]; then
-  echo "Fichier token introuvable : $TOKEN_FILE"
+  echo "Fichier token introuvable"
   exit 1
 fi
 
 K3S_TOKEN=$(cat "$TOKEN_FILE")
 
-K3S_URL="https://10.0.1.56:6443"
 
+# Désinstallation si présente
+if [ -f /usr/local/bin/k3s-agent-uninstall.sh ]; then
+  sudo /usr/local/bin/k3s-agent-uninstall.sh
+fi
 
-for HOST in "${HOSTS[@]}"; do
-  echo "Installation K3s agent sur $HOST"
+if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
+  sudo /usr/local/bin/k3s-uninstall.sh
+fi
 
-ssh -o ConnectTimeout=5 ${USER}@${HOST} "bash -s" <<EOF &
-curl -sfL https://get.k3s.io | \
-  K3S_URL=${K3S_URL} \
-  K3S_TOKEN=${K3S_TOKEN} \
+# Installation AGENT
+sudo curl -sfL https://get.k3s.io | \
+  INSTALL_K3S_EXEC="agent" \
+  K3S_URL="${K3S_URL}" \
+  K3S_TOKEN="${K3S_TOKEN}" \
   sh -
 
-# Création du fichier registries.yaml
+# Registry insecure pour containerd
 sudo mkdir -p /etc/rancher/k3s
-sudo tee /etc/rancher/k3s/registries.yaml > /dev/null <<YAML
+sudo tee /etc/rancher/k3s/registries.yaml > /dev/null <<'YAML'
 mirrors:
   "10.0.1.56:5000":
     endpoint:
       - "http://10.0.1.56:5000"
 YAML
 
-# Redémarrage de l’agent K3s
-sudo systemctl restart k3s-agent
-
-# Création du daemon.json
-sudo tee /etc/docker/daemon.json > /dev/null <<JSON
+sudo tee /etc/docker/daemon.json > /dev/null <<'YAML'
 {
   "insecure-registries" : ["10.0.1.56:5000"]
 }
-JSON
+YAML
 
-sudo systemctl restart docker
-EOF
 
-done
+systemctl restart docker
 
-wait
-echo "Installation terminée sur toutes les machines"
+sudo systemctl restart k3s-agent
+sudo systemctl status k3s-agent --no-pager
 
+
+echo "Installation terminée"
