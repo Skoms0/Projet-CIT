2677871c21ed635e5362e851b59f825cabc3052b
Fabien Denoyelle <denoyelle.fabien@gmail.com>
2026-01-22 09:07:15 +0000

    Fixed some deployment files

--- spark/deploy-spark.sh
+++ spark/deploy-spark.sh
@@ -1,6 +1,8 @@
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
 helm repo add spark-operator https://kubeflow.github.io/spark-operator
 helm repo update
-helm upgrade --install spark-operator spark-operator/spark-operator -f spark-operator-values.yaml
+helm upgrade --install spark-operator spark-operator/spark-operator -f spark-operator-values.yaml --set webhook.enable=false
 
 kubectl create sa spark
 
