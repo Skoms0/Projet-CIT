fdbb4e0ebef14da9400f7f3e49a2a756c1bd8efd
Fabien Denoyelle <denoyelle.fabien@gmail.com>
2026-01-23 08:22:19 +0000

    Updated deployment

--- tensorflow/deploy-tensorflow-app.sh
+++ tensorflow/deploy-tensorflow-app.sh
@@ -1,3 +1,3 @@
-docker buildx build -t 10.0.1.56:5000/tensorflow-app:latest --push .
+docker buildx build -t 10.0.1.56:5000/tensorflow-app:2.0.0 --push .
 
 kubectl apply -f tensorflow-app.yaml
--- web_server/deploy-web.sh
+++ web_server/deploy-web.sh
@@ -1,3 +1,6 @@
 docker buildx build -t 10.0.1.56:5000/kafka-web-combo:latest --push .
 
+kubectl label node raspberry-3b-1-13 web-disallowed=true
+
+
 kubectl apply -f k3s-web.yaml
