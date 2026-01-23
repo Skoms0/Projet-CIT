b6dbeca13ae5f9540165c5463968763baaffb803
Fabien Denoyelle <denoyelle.fabien@gmail.com>
2026-01-23 10:06:15 +0100

    Switch Redis deployment to Helm chart
    Replaced kubectl command with Helm install for Redis deployment.

--- redis/build-and-deploy.sh
+++ redis/build-and-deploy.sh
@@ -1,4 +1,9 @@
 docker buildx build -t 10.0.1.56:5000/kafka-ingestor:latest --push .
 
-kubectl apply -f redis.yaml
+#kubectl apply -f redis.yaml
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install redis bitnami/redis \
+  --set architecture=replication
+
 kubectl apply -f kafka-ingestor.yaml
+
