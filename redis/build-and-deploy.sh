c01de681930b12799c1da3ec9c00acf9dcf6e14e
Fabien Denoyelle <denoyelle.fabien@gmail.com>
2026-01-23 10:16:00 +0100

    Change Docker image tag and clean up script
    Updated Docker image tag from 'latest' to '1.0.0' and removed redundant kubectl apply command.

--- redis/build-and-deploy.sh
+++ redis/build-and-deploy.sh
@@ -1,9 +1,7 @@
-docker buildx build -t 10.0.1.56:5000/kafka-ingestor:latest --push .
-
-#kubectl apply -f redis.yaml
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install redis bitnami/redis \
-  --set architecture=replication
+docker buildx build -t 10.0.1.56:5000/kafka-ingestor:1.0.0 --push .
 
 kubectl apply -f kafka-ingestor.yaml
+
+#kubectl apply -f redis.yaml
+
 
